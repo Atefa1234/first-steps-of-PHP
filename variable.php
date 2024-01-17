dda0df10200dc130f6b09bebaa51049d25a85570
atefa1234 <atefa.mahmoodi2021@gmail.com>
2024-01-18 00:08:20 +0100

    variables added

---
+++ variable.php
@@ -0,0 +1,22 @@
+<?php
+$name = "Ana ce";
+var_dump($name);
+echo "<br>";
+$age = 35;
+var_dump($age);
+echo "<br>";
+$height = 1.60;
+var_dump($height);
+echo "<br>";
+$isLongHair =TRUE;
+var_dump($isLongHair);
+echo "<br>";
+$friends= [
+    "Atefa" => "Afghan",
+    "Yami" => "Argentine",
+    "Anuska" => "Español",
+];
+var_dump($friends);
+var_dump("hello " .$name. " welcome to our class");
+
+?>
